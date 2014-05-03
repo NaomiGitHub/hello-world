2efb6cb0a35942be87f949f8765ef33d0e6f56fa
NaomiGitHub <1020689216@qq.com>
2014-05-04 07:29:12 +0800

    please

---
+++ tttt/tttt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tttt
+//
+//  Created by 鸿妈 on 14-5-4.
+//  Copyright (c) 2014年 Naomi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ tttt/ttttTests/ttttTests.m
@@ -0,0 +1,34 @@
+//
+//  ttttTests.m
+//  ttttTests
+//
+//  Created by 鸿妈 on 14-5-4.
+//  Copyright (c) 2014年 Naomi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ttttTests : XCTestCase
+
+@end
+
+@implementation ttttTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
